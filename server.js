56b183ab9cfd9bc596cb9146d04b05ad2511231a
Luis Amaison <luis.unlam@gmail.com>
2024-12-17 16:52:23 -0300

    Iniciando proyecto en vercel

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
-const { getVoiceMembers } = require('./bot/bot'); // Correcta importación
+const { getVoiceMembers } = require('./bot/bot'); // Asegúrate de que esta importación sea válida
 const app = express();
-const PORT = 3000;
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Endpoint para obtener los miembros conectados
 app.get('/api/voice-members', (req, res) => {
-    const members = getVoiceMembers();  // Llamamos a la función para obtener los miembros actualizados
-    res.json(members);  // Responde con los miembros en formato JSON
+    const members = getVoiceMembers(); // Llamamos a la función para obtener los miembros actualizados
+    res.json(members); // Responde con los miembros en formato JSON
 });
 
 // Carga la página principal
@@ -17,4 +17,5 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Servidor web corriendo en http://localhost:${PORT}`));
+// Exporta el app para que Vercel lo maneje
+module.exports = app;
