4e55edf6798deed29126b21661d18bf9db684cf3
Luis Amaison <luis.unlam@gmail.com>
2024-12-17 21:59:13 -0300

    visualizando usuarios

--- server.js
+++ server.js
@@ -5,18 +5,26 @@ const { getVoiceMembers } = require('./bot/bot'); // Asegúrate de que esté cor
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = ['https://luisdev6.github.io', 'http://127.0.0.1:5500'];
 
-// Habilitar CORS solo para tu dominio de GitHub Pages
+// Habilitar CORS sin restricciones (o limita a tu frontend si es necesario)
 app.use(cors({
-    origin: 'https://luisdev6.github.io'
+    origin: (origin, callback) => {
+        // Permite solicitudes desde los orígenes definidos o desde clientes sin origen (como Postman)
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por la política de CORS'));
+        }
+    }
 }));
 
 // Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/voice-members', (req, res) => {
-    const members = getVoiceMembers();
-    res.json(members);
+    const members = getVoiceMembers(); // Llama a la función para obtener los usuarios
+    res.json(members); // Devuelve la lista como JSON
 });
 
 // Página principal
